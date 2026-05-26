940886da0fde14598397b67e1e14f35ba6602149
azizbekruziqulov099-ctrl <azizbekruziqulov099@gmail.com>
2026-05-26 07:16:42 +0500

    Add files via upload

---
+++ dts_handlers.py
@@ -0,0 +1,48 @@
+from aiogram import F
+from aiogram.types import (
+    Message,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton
+)
+import psycopg2
+
+from config import DATABASE_URL
+
+
+async def dts_menu(message):
+
+    conn = psycopg2.connect(
+        DATABASE_URL
+    )
+
+    cur = conn.cursor()
+
+    cur.execute("""
+        SELECT DISTINCT grade
+        FROM dts_tree
+        WHERE track='DTS'
+        ORDER BY grade
+    """)
+
+    grades = cur.fetchall()
+
+    kb = InlineKeyboardMarkup(
+        inline_keyboard=[]
+    )
+
+    for grade in grades:
+
+        kb.inline_keyboard.append([
+            InlineKeyboardButton(
+                text=f"{grade[0]}-sinf",
+                callback_data=f"dts_grade_{grade[0]}"
+            )
+        ])
+
+    await message.answer(
+        "📚 DTS sinfni tanlang:",
+        reply_markup=kb
+    )
+
+    cur.close()
+    conn.close()
