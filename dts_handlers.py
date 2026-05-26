b2a36ae23b66bf6416f52d327b6b22d678268d65
azizbekruziqulov099-ctrl <azizbekruziqulov099@gmail.com>
2026-05-26 23:47:57 +0500

    Update dts_handlers.py27.001

--- dts_handlers.py
+++ dts_handlers.py
@@ -1,3 +1,4 @@
+from aiogram.types import CallbackQuery
 from aiogram import F
 from aiogram.types import (
     Message,
@@ -42,6 +43,52 @@ async def dts_menu(message):
 
     await message.answer(
         "📚 DTS sinfni tanlang:",
+        reply_markup=kb
+    )
+
+    cur.close()
+    conn.close()
+async def dts_grade(call: CallbackQuery):
+
+    grade = call.data.replace(
+        "dts_grade_",
+        ""
+    )
+
+    conn = psycopg2.connect(
+        DATABASE_URL
+    )
+
+    cur = conn.cursor()
+
+    cur.execute(
+        """
+        SELECT DISTINCT subject
+        FROM dts_tree
+        WHERE track='DTS'
+        AND grade=%s
+        ORDER BY subject
+        """,
+        (grade,)
+    )
+
+    subjects = cur.fetchall()
+
+    kb = InlineKeyboardMarkup(
+        inline_keyboard=[]
+    )
+
+    for subject in subjects:
+
+        kb.inline_keyboard.append([
+            InlineKeyboardButton(
+                text=subject[0],
+                callback_data=f"dts_subject_{grade}_{subject[0]}"
+            )
+        ])
+
+    await call.message.edit_text(
+        f"{grade}-sinf fanlari",
         reply_markup=kb
     )
 
