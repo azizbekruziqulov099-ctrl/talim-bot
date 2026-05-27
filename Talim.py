66ba842e03ee4712f25a49f414f7bc7c63ba6493
azizbekruziqulov099-ctrl <azizbekruziqulov099@gmail.com>
2026-05-27 07:40:33 +0500

    Update Talim.py

--- Talim.py
+++ Talim.py
@@ -6,6 +6,7 @@ from aiogram.types import ReplyKeyboardRemove
 from aiogram import Bot, Dispatcher, types
 from urllib.parse import quote
 from aiogram.filters import *
+from keyboards import get_main_keyboard
 from aiogram import F
 from aiogram.types import (
     ReplyKeyboardMarkup,
@@ -334,51 +335,6 @@ def question_nav_keyboard(test):
                 KeyboardButton(text=FINISH)
             ]
         ],
-        resize_keyboard=True
-    )
-
-def get_main_keyboard(role=None):
-
-    # O‘quvchi menyusi
-    if role == "O‘quvchi":
-        keyboard = [
-            [KeyboardButton(text="📚 BILIMNI SINASH"),
-             KeyboardButton(text="📚 DTS")],
-            [KeyboardButton(text="🎓 Sinf statistikasi"),
-            KeyboardButton(text="🏫 Maktab statistikasi")],
-            [KeyboardButton(text="📊 So‘rovnoma"),
-            KeyboardButton(text="⚙️ Akkaunt sozlamalari")]
-        ]
-    # O‘qituvchi menyusi
-    elif role == "O‘qituvchi":
-            keyboard = [
-                [KeyboardButton(text="🧠 Bilimni sinash"),
-                KeyboardButton(text="📊 Bilim darajam")],
-                [KeyboardButton(text="👨‍🎓 O‘quvchilar natijasi"),
-                KeyboardButton(text="🏫 Maktab statistikasi")],
-                [KeyboardButton(text="🌍 Viloyat statistikasi"),
-                KeyboardButton(text="📊 So‘rovnoma")],
-                [KeyboardButton(text="⚙️ Akkaunt sozlamalari")]
-            ]
-    elif role == "Admin":
-
-        keyboard = [
-            [KeyboardButton(text="🇺🇿 Respublika statistikasi"),
-            KeyboardButton(text="📚 DTS"),
-            KeyboardButton(text="🏫 Maktab statistikasi")],
-            [KeyboardButton(text="🎓 Sinf statistikasi"),
-            KeyboardButton(text="👨‍🎓 TOP o‘quvchilar"),
-            KeyboardButton(text="👨‍🏫 TOP o‘qituvchilar")],
-            [KeyboardButton(text="⚙️ Akkaunt sozlamalari"),
-            KeyboardButton(text="📋 So‘rovnoma natijalari"),
-            KeyboardButton(text="📚 BILIMNI SINASH bazasi")],
-            [KeyboardButton(text="👥 Foydalanuvchilar statistikasi"),
-            KeyboardButton(text="📚 DTS boshqaruvi")]
-        ]
-    
-
-    return ReplyKeyboardMarkup(
-        keyboard=keyboard,
         resize_keyboard=True
     )
 
