99629997fd2d4f13cd7af835a784fcac585cc170
azizbekruziqulov099-ctrl <azizbekruziqulov099@gmail.com>
2026-05-28 15:25:56 +0500

    Update dts_import_handlers.py

--- dts_import_handlers.py
+++ dts_import_handlers.py
@@ -1395,8 +1395,7 @@ async def dts_import(
 ):
 
     await state.set_state(
-        DTSImportState.waiting_excel,
-        F.document
+        DTSImportState.waiting_excel
     )
 
     kb = InlineKeyboardMarkup(
