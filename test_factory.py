653f889ce3f9c37d5028fba1a70e58efa62ef23e
azizbekruziqulov099-ctrl <azizbekruziqulov099@gmail.com>
2026-05-31 14:29:29 +0500

    Update test_factory.py

--- test_factory.py
+++ test_factory.py
@@ -1,10 +1,22 @@
-from topic_generation import get_next_topic
+from topic_generation import (
+    get_next_topic,
+    increase_count
+)
 
 topics = get_next_topic(10)
 
 for topic in topics:
+
     topic_code = topic[0]
 
     print(
-        f"Test yaratilmoqda: {topic_code}"
+        f"Yaratilmoqda: {topic_code}"
+    )
+
+    # keyin AI generator keladi
+
+    increase_count(topic_code)
+
+    print(
+        f"Tugadi: {topic_code}"
     )
