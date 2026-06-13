60ec4c6a24f16cb2add73c5d8270646e959d4a34
azizbekruziqulov099-ctrl <azizbekruziqulov099@gmail.com>
2026-06-13 11:35:17 +0500

    Update test_engine.py

--- test_engine.py
+++ test_engine.py
@@ -95,7 +95,7 @@ async def show_question(
             and str(image_url).lower() != "nan"
             and str(image_url).strip() != ""
         ):
-
+            print("IMAGE =", repr(image_url))
             await message.answer_photo(
                 photo=image_url,
                 caption=
