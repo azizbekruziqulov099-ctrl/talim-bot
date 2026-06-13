e98ae62da319c306e35af7f577a18a3d4bd47dee
azizbekruziqulov099-ctrl <azizbekruziqulov099@gmail.com>
2026-06-13 23:10:10 +0500

    Add user roles, education types, and keyboard function
    Added constants for user roles, education types, school types, and class letters. Implemented a function to create a keyboard with buttons for given items.

--- https:/register.py
+++ https:/register.py
@@ -1,1 +1,37 @@
+from aiogram.types import (
+    ReplyKeyboardMarkup,
+    KeyboardButton
+)
 
+user_state = {}
+temp_user = {}
+
+ROLES = [
+    "🧒 O‘quvchi",
+    "👨‍🏫 O‘qituvchi"
+]
+
+EDUCATION_TYPES = [
+    "👶 Maktabgacha",
+    "🏫 Maktab"
+]
+
+SCHOOL_TYPES = [
+    "🏫 Oddiy maktab",
+    "⭐ Ixtisoslashtirilgan maktab",
+    "🇺🇿 Prezident maktabi",
+    "🧮 Al-Xorazmiy maktabi",
+    "🪖 Harbiy maktab",
+    "🎨 San'at maktabi",
+    "📖 IDUM"
+]
+
+CLASS_LETTERS = [
+    "A", "B", "C", "D", "E"
+]
+
+def make_keyboard(items):
+    return ReplyKeyboardMarkup(
+        keyboard=[[KeyboardButton(text=i)] for i in items],
+        resize_keyboard=True
+    )
