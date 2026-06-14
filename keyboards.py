4585228f37bf972e98297ded4d2e88e39d8fee39
azizbekruziqulov099-ctrl <azizbekruziqulov099@gmail.com>
2026-06-14 10:25:57 +0500

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -5,44 +5,52 @@ from aiogram.types import (
 
 def get_main_keyboard(role=None):
 
-    # O‘quvchi menyusi
-    if role == "O‘quvchi":
+    if "O‘quvchi" in str(role):
+
         keyboard = [
             [KeyboardButton(text="📚 BILIMNI SINASH"),
              KeyboardButton(text="📚 DTS Navigator")],
+
             [KeyboardButton(text="🎓 Sinf statistikasi"),
-            KeyboardButton(text="🏫 Maktab statistikasi")],
+             KeyboardButton(text="🏫 Maktab statistikasi")],
+
             [KeyboardButton(text="📊 So‘rovnoma"),
-            KeyboardButton(text="⚙️ Akkaunt sozlamalari")]
+             KeyboardButton(text="⚙️ Akkaunt sozlamalari")]
         ]
-    # O‘qituvchi menyusi
-    elif role == "O‘qituvchi":
-            keyboard = [
-                [KeyboardButton(text="🧠 Bilimni sinash"),
-                KeyboardButton(text="📊 Bilim darajam")],
-                [KeyboardButton(text="👨‍🎓 O‘quvchilar natijasi"),
-                KeyboardButton(text="🏫 Maktab statistikasi")],
-                [KeyboardButton(text="🌍 Viloyat statistikasi"),
-                KeyboardButton(text="📊 So‘rovnoma")],
-                [KeyboardButton(text="⚙️ Akkaunt sozlamalari")]
-            ]
+
+    elif "O‘qituvchi" in str(role):
+
+        keyboard = [
+            [KeyboardButton(text="🧠 Bilimni sinash"),
+             KeyboardButton(text="📊 Bilim darajam")],
+
+            [KeyboardButton(text="👨‍🎓 O‘quvchilar natijasi"),
+             KeyboardButton(text="🏫 Maktab statistikasi")],
+
+            [KeyboardButton(text="🌍 Viloyat statistikasi"),
+             KeyboardButton(text="📊 So‘rovnoma")],
+
+            [KeyboardButton(text="⚙️ Akkaunt sozlamalari")]
+        ]
     elif role == "Admin":
 
         keyboard = [
             [KeyboardButton(text="🇺🇿 Respublika statistikasi"),
             KeyboardButton(text="🤖 AI Generator"),
             KeyboardButton(text="🏫 Maktab statistikasi")],
+
             [KeyboardButton(text="🎓 Sinf statistikasi"),
             KeyboardButton(text="👨‍🎓 TOP o‘quvchilar"),
             KeyboardButton(text="👨‍🏫 TOP o‘qituvchilar")],
+
             [KeyboardButton(text="⚙️ Akkaunt sozlamalari"),
             KeyboardButton(text="📋 So‘rovnoma natijalari"),
             KeyboardButton(text="📚 BILIMNI SINASH bazasi")],
+
             [KeyboardButton(text="👥 Foydalanuvchilar statistikasi"),
             KeyboardButton(text="📚 DTS boshqaruvi"),
             KeyboardButton(text="🤖 Test generator")]
         ]
-    
 
     return ReplyKeyboardMarkup(
         keyboard=keyboard,
