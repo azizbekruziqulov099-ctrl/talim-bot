9387879a32de4b365d75653d1bf51f46bc73b964
azizbekruziqulov099-ctrl <azizbekruziqulov099@gmail.com>
2026-06-14 21:31:35 +0500

    Update loader.py

--- loader.py
+++ loader.py
@@ -1,5 +1,7 @@
 from aiogram import Bot, Dispatcher
 import os
+from aiogram.types import CallbackQuery
+from aiogram_calendar import SimpleCalendar, SimpleCalendarCallback
 
 API_TOKEN = os.getenv(
     "BOT_TOKEN"
@@ -10,3 +12,37 @@ bot = Bot(
 )
 
 dp = Dispatcher()
+
+@dp.callback_query(SimpleCalendarCallback.filter())
+async def process_simple_calendar(
+    callback_query: CallbackQuery,
+    callback_data: SimpleCalendarCallback
+):
+
+    selected, date = await SimpleCalendar().process_selection(
+        callback_query,
+        callback_data
+    )
+
+    if selected:
+
+        user_id = callback_query.from_user.id
+
+        temp_user[user_id]["birth_date"] = date.strftime(
+            "%d.%m.%Y"
+        )
+
+        user_state[user_id] = "gender"
+
+        await callback_query.message.edit_text(
+            reg_status(temp_user[user_id]) +
+            "\n\n👤 Jinsni tanlang:"
+        )
+
+        await callback_query.message.answer(
+            "👤 Jinsni tanlang:",
+            reply_markup=make_keyboard([
+                "👨 Erkak",
+                "👩 Ayol"
+            ])
+        )
